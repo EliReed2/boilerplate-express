ec505b77e5f0e8227ccce5317aa06f691c20b404
Eli Reed <elireed1414@gmail.com>
2024-07-09 13:24:53 +0000

    e

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-absolutePath = __dirname + '/relativePath/file.ext';
+absolutePath = __dirname + '/views/index.html';
 app.get("/", function(req, res) {
     res.send("Hello Express");
     res.sendFile(absolutePath);
